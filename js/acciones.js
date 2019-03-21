4ac4ab59bd3a384e8a6f84cbadd9f71a134992f0
Evelia Reyes <eveliacbtis15@gmail.com>
2019-03-21 13:31:00 -0600

    4

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 
 $(document).ready(function(e){
-	document .addEventListener("deviceready",function(){
+	document.addEventListener("deviceready",function(){
 		$('#disp table td').eq(3).text(device.model);
 		$('#disp table td').eq(5).text(device.cordava);
 		$('#disp table td').eq(7).text(device.platform);
